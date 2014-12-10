4218dd89ffc3b2a53206a6bacf9e16f854882f29
Shou Ya <shouyatf@gmail.com>
2014-12-10 12:54:09 +0800

    worked but still buggy

--- app.rb
+++ app.rb
@@ -1,7 +1,39 @@
 require 'sinatra'
 require 'json'
+require 'rest-client'
+require 'time'
 
 require 'sinatra/reloader' if development?
+
+REQUEST_HOST = 'https://meiyanchu.com'
+# REQUEST_HOST = 'http://127.0.0.1:3000'
+PERFORMANCE_ID = 5547
+# PERFORMANCE_ID = 1
+
+REQUEST_PATH = "/api/performances/#{PERFORMANCE_ID}/comments"
+
+helpers do
+  def fetch_comments
+    request_uri = "#{REQUEST_HOST}#{REQUEST_PATH}"
+    params = {
+      without_photos: true
+    }
+
+    params[:since] = @last_fetch.iso8601 if @last_fetch
+
+    puts params
+
+    resp = RestClient.get(request_uri,
+                          params: params)
+    result = JSON.parse(resp)
+    if result.empty?
+      []
+    else
+      @last_fetch = Time.now
+      result.reverse
+    end
+  end
+end
 
 get '/' do
   send_file 'public/danmaku.html', type: :html
@@ -12,10 +44,17 @@ get '/danmaku.json' do
 
   stream do |out|
     loop do
-      text = 'a' * (rand * 10).to_i
-
-      out << JSON.dump({danmaku: {text: "meow~ #{text}"}})
-      sleep (rand * 3)
+      fetch_comments.each do |cmt|
+        puts cmt['content']
+        json = {
+          danmaku: {
+            text: cmt['content']
+          }
+        }
+        out << JSON.dump(json)
+        sleep 0.1
+      end
+      sleep 1
     end
   end
 end
