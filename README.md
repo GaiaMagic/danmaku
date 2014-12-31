80006ec127a432281f6fcc21dca381786743c15f
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-12-31 17:26:19 +0800

    rewrite

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ manual
 
 Simply run `fig up -d` to start this application.
 
+If you have changed the code and want to rebuild the app, run
+`fig build && fig kill && fig up -d`.
+
 To install `fig`, follow <http://www.fig.sh/>.
 
 nginx configuration
@@ -20,4 +23,14 @@ location ~* ^/show/(\d+)/danmu {
 	proxy_http_version 1.1;
 	chunked_transfer_encoding off;
 }
+```
+
+manually adding a comment
+-------------------------
+
+```
+curl https://youyanchu.com/api/performances/<SHOW_ID>/comments \
+-X POST -d '{"content": "hahaha", "is_anonymous": "true"}' \
+-H 'content-type: application/json' \
+-H 'x-user-access-token: <YOUR_ACCESS_TOKEN>'
 ```
--- public/danmaku.js
+++ public/danmaku.js
@@ -1,19 +1,10 @@
 var board = document.getElementById('board');
 
-var speed         = 2.5 * 1000;
-var scroll_delay  = 30;
-var danmaku_lines = 15;
-
-var calculate_text_width = function(text, font_height) {
+var calculate_text_width = function(text) {
     var test = document.createElement('div');
 
-    test.style.fontSize   = font_height + 'px';
-    test.style.lineHeight = font_height + 'px';
-    test.style.position   = 'absolute';
-    test.style.height     = 'auto';
-    test.style.width      = 'auto';
+    test.setAttribute('class', 'danmaku');
     test.style.visibility = 'hidden';
-    test.style.whiteSpace = 'nowrap';
     test.textContent      = text;
 
     board.appendChild(test);
@@ -23,61 +14,67 @@ var calculate_text_width = function(text, font_height) {
 
     test.remove();
 
-    return new Object({
+    return {
         width:  width,
         height: height
-    });
+    };
 };
 
+var ROWS = [];
+
 var build_danmaku = function(text) {
-    var screen_width  = window.innerWidth;
     var screen_height = window.innerHeight;
     var dmk           = document.createElement('div');
-    var font_height   = screen_height / danmaku_lines;
-    var font_size     = calculate_text_width(text, font_height);
+    var font_size     = calculate_text_width(text);
 
-    dmk.style.fontSize   = font_height + 'px';
-    dmk.style.lineHeight = font_height + 'px';
-    dmk.textContent      = text;
+    dmk.textContent = text;
     dmk.setAttribute('class', 'danmaku');
 
     board.appendChild(dmk);
 
-    var top = (screen_height - font_size.height - 1) * Math.random();
-    var scroll_amount = (screen_width + font_size.width) / speed * scroll_delay;
+    var heightPerRow = font_size.height;
+    var maxRows = Math.floor(screen_height / heightPerRow);
+
+    var row;
+    for (var i = 0; i < maxRows; i++) {
+        if (!ROWS[i] || ROWS[i].length === 0) {
+            row = i;
+            break;
+        }
+    }
+
+    if (typeof row === 'undefined') {
+        row = Math.floor(maxRows * Math.random());
+    }
+
+    var top = row * heightPerRow;
+
+    var delay = 0;
+    if (ROWS[row] && ROWS[row].length > 0) {
+        delay = +ROWS[row][ROWS[row].length - 1].getAttribute('death') - +new Date();
+        console.log(delay, 'delayed', ROWS[row][ROWS[row].length - 1].getAttribute('death'), +ROWS[row][ROWS[row].length - 1].getAttribute('death'))
+    }
+
+    ROWS[row] = ROWS[row] || [];
+    ROWS[row].push(dmk);
 
     dmk.style.top = top + 'px';
+    dmk.style.right = '-' + font_size.width + 'px';
 
-    dmk.setAttribute('scrollamount' , scroll_amount);
-    dmk.setAttribute('direction'    , 'left');
-    dmk.setAttribute('left'         , screen_width);
-    dmk.setAttribute('remove_at'    , 0 - screen_width - font_size.width);
-    dmk.style.left = screen_width + 'px';
+    dmk.style.transitionDelay = delay + 'ms';
+    dmk.style.transitionDuration = '10s';
 
-    dmk.style.visibility = 'visible';
+    setTimeout(function () {
+        dmk.setAttribute('class', 'danmaku playing');
+        dmk.setAttribute('birth', +new Date() + delay);
+        dmk.setAttribute('death', +new Date() + delay + 12000);
+    }, 0);
 
-    return dmk;
-};
-
-var setup_timer = function (){
-    window.setInterval(function() {
-        var dead_danmaku = [];
-        for (var i = 0; i < board.children.length; ++i) {
-            var dmk = board.children[i];
-            var new_left = dmk.getAttribute('left') -
-                    dmk.getAttribute('scrollamount');
-            dmk.setAttribute('left', new_left);
-            dmk.style.left = new_left + 'px';
-
-            if (new_left < dmk.getAttribute('remove_at')) {
-                dead_danmaku.push(dmk);
-            }
-        }
-
-        while (dead_danmaku.length > 0) {
-            dead_danmaku.pop().remove();
-        }
-    }, scroll_delay);
+    var CUR = ROWS[row];
+    setTimeout(function () {
+        CUR.splice(CUR.indexOf(dmk), 1);
+        dmk.parentNode.removeChild(dmk);
+    }, 12000 + delay);
 };
 
 var evtSource = new EventSource('stream.json');
@@ -89,6 +86,5 @@ document.onreadystatechange = function (){
             var node = JSON.parse(e.data);
             build_danmaku(node.text);
         }, false);
-        setup_timer();
     }
 };
